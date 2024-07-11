9136a3fe9a7983f7a81830d2f275f66215450801
Venfah <32269227+venfah@users.noreply.github.com>
2024-07-11 18:30:04 +0530

    Created using Colab

---
+++ ai/prediction-1.py
@@ -0,0 +1,15 @@
+{
+  "cells": [],
+  "metadata": {
+    "colab": {
+      "name": "beginner.ipynb",
+      "provenance": []
+    },
+    "kernelspec": {
+      "display_name": "Python 3",
+      "name": "python3"
+    }
+  },
+  "nbformat": 4,
+  "nbformat_minor": 0
+}
